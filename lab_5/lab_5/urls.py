972452dd7758ff011e53880e5e955fa04af9a23b
Alex <al.gorzhanin23@gmail.com>
2022-06-15 19:16:37 +0300

    Change urls data

--- lab_5/lab_5/urls.py
+++ lab_5/lab_5/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Tourism.urls')),
-    path('tstpage', HttpResponse("google.com"))
+    path('tstpage', HttpResponse("yandex.ru"))
 ]
